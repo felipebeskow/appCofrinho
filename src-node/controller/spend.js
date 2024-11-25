df9b5cb8f81958362a4c001d6574f5fde7200e8c
Felipe Helfensteler Beskow <felipebeskow@outlook.com>
2024-11-25 15:47:23 -0300

    Ajuste conexão banco de dados

--- src-node/controller/spend.js
+++ src-node/controller/spend.js
@@ -2,26 +2,6 @@ const DbConnection = require("../db/conection")
 
 module.exports = {
     async post(req, res) {
-        let query = req.body
-        try {
-            //let result = await DbConnection.raw(`SELECT VERSION()`)
-            await DbConnection.transaction(async (trx) => {
-                const result = await trx('CATEGORIAS')
-                    .insert({
-                        CAT_NOME: query.nome,
-                        CAT_DESCRICAO: query.descricao,
-                    })
-                    .returning(['CAT_UUID', 'CAT_NOME', 'CAT_DESCRICAO'])
-
-                await trx.commit() // Confirma a transação
-                console.log(result)
-                res.json(result)
-            })
-        } catch (error) {
-            console.error("Error inserting data:", error);
-            res.status(500).json({
-                error: "Failed to insert data"
-            });
-        }
+        
     }
 }
--- src-node/db/conection.js
+++ src-node/db/conection.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const fs = require('fs');
 require("dotenv").config();
 let DbConnection = knex({
-    client: "mysql",
+    client: "mysql2",
     connection: {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
