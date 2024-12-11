4b7b9e672ef280d2259bf62a6f431de24f12c787
Felipe Helfensteler Beskow <felipebeskow@outlook.com>
2024-12-11 18:06:27 -0300

    Ajuste envio de arquivos

--- src-node/controller/fileUpload.js
+++ src-node/controller/fileUpload.js
@@ -3,42 +3,57 @@ const { Readable } = require('stream');
 const path = require('path');
 
 // Carregar as credenciais do seu projeto
-const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, '/../credenciais.json'),
-    scopes: ['https://www.googleapis.com/auth/drive']
-});
 
-const drive = google.drive({ version: 'v3', auth });
+
+const GOOGLE_API_FOLDER_ID = '0B_8InX3YWZabTE1nSkUzeEg3U1U'
+
+
 module.exports = {
     async post(req, res) {
-        console.log(req.file)
         try {
+            
+            const auth = new google.auth.GoogleAuth({
+                keyFile: path.join(__dirname, '/../credenciais.json'),
+                scopes: ['https://www.googleapis.com/auth/drive']
+            });
+            
+            const driveService = google.drive({
+                version: 'v3',
+                auth 
+            });
+
             const fileName = req.file.originalname;
-            const fileBuffer = req.file.buffer;
 
             const fileMetadata = {
-                name: fileName
-            };
+                name: fileName,
+                parents: [GOOGLE_API_FOLDER_ID]
+            }
 
+            const fileBuffer = req.file.buffer;
+            
             const readableStream = new Readable();
             readableStream.push(fileBuffer);
             readableStream.push(null);
 
             const media = {
-                mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                mimeType: 'image/jpg',
                 body: readableStream
-            };
+            }
 
-            const response = await drive.files.create({
-                resource: fileMetadata,
-                media: media,
+            const response = await driveService.files.create({
+                resource:fileMetadata,
+                media,
                 fields: 'id'
-            });
+            })
 
-            res.send(`Arquivo enviado com sucesso: ${response.data.id}`);
-        } catch (err) {
-            console.error('Erro ao enviar o arquivo:', err);
-            res.status(500).send('Erro ao enviar o arquivo');
+            console.log(response.data.id)
+
+        } catch (error) {
+            console.error('Erro ao enviar o arquivo:', error);
+            res.status(500).json({
+                message:'Erro ao enviar o arquivo',
+                error
+            });
         }
     },
 }
